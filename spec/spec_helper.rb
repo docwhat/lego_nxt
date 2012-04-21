8387238195ff9f61aefc9d8b15efa03a6db7cef6
Christian Höltje <docwhat@gerf.org>
2012-04-20 22:18:05 -0400

    Typo: NXN -> NXT

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ HAS_NXT = ENV['HAS_NXT'] == 'true'
 
 if not HAS_NXT
   puts "**NOTE** Some tests are marked pending because you did not indicate you have a NXT brick."
-  puts "         To run all tests, plugin a NXN brick via USB, power it on, and set"
+  puts "         To run all tests, plugin a NXT brick via USB, power it on, and set"
   puts "         the environment variable 'HAS_NXT' to 'true'."
 end
 
