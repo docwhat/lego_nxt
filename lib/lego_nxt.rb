a570794a24c5d2ab107d8d12b736886b41608837
Christian Höltje <docwhat@gerf.org>
2012-04-22 16:14:17 -0400

    Added LegoNXT::connect method
    This is handy method to connect to any brick it can find.  I'll
    fill this out when I add bluetooth so it'll do everything as
    automatically as it can.

--- lib/lego_nxt.rb
+++ lib/lego_nxt.rb
@@ -1,5 +1,16 @@
 require "lego_nxt/version"
+require "lego_nxt/brick"
+require "lego_nxt/usb_connection"
 
 # {LegoNXT} is a library for working with the LEGO NXT bricks.
 module LegoNXT
+end
+
+# A helper method that attempt to attach to a brick and
+# returns a {LegoNXT::Brick object}.
+#
+# @return [LegoNXT::Brick]
+def LegoNXT::connect
+  @found_connection ||= LegoNXT::UsbConnection.new
+  LegoNXT::Brick.new @found_connection
 end
