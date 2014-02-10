4fa971a32b00623cc621580b34d1bb0019638d60
Jonan Scheffler <jonanscheffler@gmail.com>
2014-02-09 21:10:10 -0800

    Remove unused test.

--- spec/lib/lego_nxt/low_level/brick_spec.rb
+++ spec/lib/lego_nxt/low_level/brick_spec.rb
@@ -133,13 +133,6 @@ describe LegoNXT::LowLevel::Brick  do
     end
   end
 
-#   describe ".light_sensor" do
-#     it "returns something" do
-#       connection.stub(:transceive) { bytestring 0x02, LegoNXT::LowLevel::DirectOps::LSREAD, 0x00, uword(1234) }
-#       subject.light_sensor.should == 1234
-#     end
-#   end
-
   describe ".reset_motor_position" do
     [:a, :b, :c].each do |port|
       [true, false].each do |relative|
